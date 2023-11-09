6df4a40bbc3f0cabb5b684f68912173c62ec47d1
Marc-Eric Boury <marc-eric.boury@collegelasalle.com>
2023-11-09 15:06:34 -0500

    complété Utilisateur et Role DTOs

--- DataAccess/DTOs/Role.cs
+++ DataAccess/DTOs/Role.cs
@@ -1,4 +1,5 @@
 ﻿using _420DA3_07451_Projet_Initial.DataAccess.DTOs.Abstracts;
+using Microsoft.VisualBasic.ApplicationServices;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,4 +9,32 @@ using System.Threading.Tasks;
 namespace _420DA3_07451_Projet_Initial.DataAccess.DTOs;
 public class Role : AbstractDTO<int> {
 
+    public const int ADMINISTRATOR_ROLE_ID = 1;
+    public const int OFFICE_EMPLOYEE_ROLE_ID = 2;
+    public const int WAREHOUSE_EMPLOYEE_ROLE_ID = 3;
+
+
+    public string RoleName { get; set; } = null!;
+    public string? RoleDescription { get; set; }
+    public byte[] RowVersion { get; set; } = null!;
+
+    public List<Utilisateur> UtplisateursPossedantRole { get; set; } = null!;
+
+
+    public Role() : base() { }
+
+    public Role(string roleName) : this() {
+        this.RoleName = roleName;
+    }
+
+    public Role(string roleName, string? roleDescription)
+        : this(roleName) {
+        this.RoleDescription = roleDescription;
+    }
+
+    protected Role(int id, string roleName, string? roleDescription, byte[] rowVersion)
+        : this(roleName, roleDescription) {
+        this.Id = id;
+        this.RowVersion = rowVersion;
+    }
 }
--- DataAccess/DTOs/Utilisateur.cs
+++ DataAccess/DTOs/Utilisateur.cs
@@ -8,4 +8,33 @@ using System.Threading.Tasks;
 namespace _420DA3_07451_Projet_Initial.DataAccess.DTOs;
 public class Utilisateur : AbstractDTO<int> {
 
+    public string Username { get; set; } = null!;
+    public string PasswordHash { get; set; } = null!;
+    public int? EntrepotDeTravailId { get; set; }
+    public DateTime DateCreated { get; set; }
+    public byte[] RowVersion { get; set; } = null!;
+
+
+    public Entrepot EntrepotDeTravail { get; set; } = null!;
+    public List<Role> Roles { get; set; } = null!;
+
+
+    public Utilisateur() : base() { }
+
+    public Utilisateur(string username, string passwordHash) : base() {
+        this.Username = username;
+        this.PasswordHash = passwordHash;
+    }
+
+    public Utilisateur(string username, string passwordHash, int? entrepotDeTravailId)
+        : this(username, passwordHash) {
+        this.EntrepotDeTravailId = entrepotDeTravailId;
+    }
+
+    protected Utilisateur(int id, string username, string passwordHash, int? entrepotDeTravailId, DateTime dateCreated, byte[] rowVersion)
+        : this(username, passwordHash, entrepotDeTravailId) {
+        this.Id = id;
+        this.DateCreated = dateCreated;
+        this.RowVersion = rowVersion;
+    }
 }
