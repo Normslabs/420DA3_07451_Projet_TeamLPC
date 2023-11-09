3ffb74999cae928c1df6b31b3afe505ef45b706c
Wolfmen421 <portugal_2000f@hotmail.com>
2023-11-09 13:50:47 -0500

    Ajout de PurchaseOrder.cs Ajout de Produit.cs

---
+++ DataAccess/DTOs/PurchaseOrder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _420DA3_07451_Projet_Initial.DataAccess.DTOs.Abstracts;
+
+namespace _420DA3_07451_Projet_Initial.DataAccess.DTOs;
+internal class PurchaseOrder : AbstractDTO<int> {
+
+
+}
