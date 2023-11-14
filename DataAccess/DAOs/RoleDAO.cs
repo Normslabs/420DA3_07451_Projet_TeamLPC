72ec829d3746a770f0123b8e8cf5e9f973c4a75b
Marc-Eric Boury <marc-eric.boury@collegelasalle.com>
2023-11-14 13:26:42 -0500

    Ajout RoleDAO

---
+++ DataAccess/DAOs/RoleDAO.cs
@@ -0,0 +1,29 @@
+﻿using _420DA3_07451_Projet_Initial.DataAccess.Contexts.Abstracts;
+using _420DA3_07451_Projet_Initial.DataAccess.DAOs.Abstracts;
+using _420DA3_07451_Projet_Initial.DataAccess.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _420DA3_07451_Projet_Initial.DataAccess.DAOs;
+public class RoleDAO : AbstractDao<Role, int> {
+    protected override AbstractContext Context { get; }
+
+    public RoleDAO(AbstractContext context) { 
+        this.Context = context;
+    }
+
+    public Role GetAdminRole() {
+        return this.GetById(Role.ADMINISTRATOR_ROLE_ID) ?? throw new Exception("Role d'administrateur nonexistant.");
+    }
+
+    public Role GetOfficeEmployeeRole() {
+        return this.GetById(Role.OFFICE_EMPLOYEE_ROLE_ID) ?? throw new Exception("Role d'employé de bureau nonexistant.");
+    }
+
+    public Role GetWarehouseEmployeeRole() {
+        return this.GetById(Role.WAREHOUSE_EMPLOYEE_ROLE_ID) ?? throw new Exception("Role d'employe d'entrepôt nonexistant.");
+    }
+}
