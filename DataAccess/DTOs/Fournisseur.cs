bb067047d79c23dfbcce38c520d872edddccc442
Olivier Kingsley <Olivier.Kingsley@gmail.com>
2023-11-14 10:38:39 -0500

    Creating DTO Supplier (fournisseur) et Shipment

--- DataAccess/DTOs/Fournisseur.cs
+++ DataAccess/DTOs/Fournisseur.cs
@@ -7,4 +7,21 @@ using System.Threading.Tasks;
 
 namespace _420DA3_07451_Projet_Initial.DataAccess.DTOs;
 public class Fournisseur : AbstractDTO<int> {
+
+
+    public int AdressId { get; set; }
+
+    public int ContactId { get; set; }
+
+    public List<Product> Products { get; set; }
+
+    public byte RowVersion { get; set; }
+
+    public Adresse SupplierAdresse { get; set; } 
+
+    public Contact SupplierContact { get; set; }
+
+    public string SupplierName { get; set; }
+
+
 }
--- DataAccess/DTOs/Shipment.cs
+++ DataAccess/DTOs/Shipment.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using _420DA3_07451_Projet_Initial.DataAccess.DTOs.Abstracts;
@@ -8,4 +9,15 @@ using Microsoft.Identity.Client;
 
 namespace _420DA3_07451_Projet_Initial.DataAccess.DTOs;
 public class Shipment : AbstractDTO<int> {
+
+    public byte RowVersion { get; set; } 
+
+    public object Service {  get; set; } : ShipmentServiceEnum;
+
+    public object  ShippingOrder { get; set; } : ShippingOrder;
+
+    public int ShippingOrderID { get; set; } 
+
+    public string TrackingNumber { get; set; } 
+
 }
