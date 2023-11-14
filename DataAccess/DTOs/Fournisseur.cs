4ac3ae7d109846ed8b20c0f1ad477e5421ec983d
Olivier Kingsley <Olivier.Kingsley@gmail.com>
2023-11-14 11:46:56 -0500

    Modification DTO fournisseur et Shipment incluant les constructeurs

--- DataAccess/DTOs/Fournisseur.cs
+++ DataAccess/DTOs/Fournisseur.cs
@@ -1,27 +1,68 @@
 ﻿using _420DA3_07451_Projet_Initial.DataAccess.DTOs.Abstracts;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static _420DA3_07451_Projet_Initial.DataAccess.DTOs.ShipmentDTO;
 
 namespace _420DA3_07451_Projet_Initial.DataAccess.DTOs;
 public class Fournisseur : AbstractDTO<int> {
 
 
-    public int AdressId { get; set; }
+    public int AdresseId { get; set; } 
 
-    public int ContactId { get; set; }
+    public int ContactId { get; set; } 
 
-    public List<Product> Products { get; set; }
+    public List<Produit> Produit { get; set; } = null!;
 
-    public byte RowVersion { get; set; }
+    public byte[] RowVersion { get; set; } = null!;
 
-    public Adresse SupplierAdresse { get; set; } 
+    public Adresse SupplierAdresse { get; set; } = null!;
 
-    public Contact SupplierContact { get; set; }
 
-    public string SupplierName { get; set; }
+    public string SupplierName { get; set; } = null!;
+
+
+    public const int SUPPLIER_NAME_MAXLENGHT = 100;
+
+
+
+
+    #region Constructeur
+    public Fournisseur() : base() {
+        
+    }
+
+    public Fournisseur(int AdresseId, int ContactId, string SupplierName) {
+        this.AdresseId = AdresseId;
+        this.ContactId = ContactId;
+        this.SupplierName = SupplierName;
+        
+    }
+
+    protected Fournisseur(int Id, int AdresseId, int ContactId, byte[] RowVersion, string SupplierName) {
+        this.Id = Id;
+        this.AdresseId = AdresseId;
+        this.ContactId = ContactId;
+        this.RowVersion = RowVersion;
+        this.SupplierName = SupplierName;
+    }
+
+    #endregion
+
+
+
+    #region Validation
+
+    public static bool ValiderSupplierName (string SupplierName) {
+        return SupplierName.Length <= SUPPLIER_NAME_MAXLENGHT;
+    }
+
+
+    #endregion
+
 
 
 }
---
+++ DataAccess/DTOs/ShipmentDTO.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using _420DA3_07451_Projet_Initial.DataAccess.DTOs.Abstracts;
+using Microsoft.Identity.Client;
+
+namespace _420DA3_07451_Projet_Initial.DataAccess.DTOs;
+public class ShipmentDTO : AbstractDTO<int> {
+
+    public byte[] RowVersion { get; set; } = null!;
+
+    public ShipmentServiceEnum Service {  get; set; } 
+
+    public ShipmentOrderDTO ShippingOrder { get; set; } = null!;
+
+    public int? ShippingOrderID { get; set; } = null!;
+
+    public string? TrackingNumber { get; set; } = null!;
+
+
+    public ShipmentDTO() : base() { 
+
+    }
+
+    public ShipmentDTO(int ShippingId, ShipmentServiceEnum Service, string TrackNumber) {
+        this.ShippingOrderID = ShippingId;
+        this.Service = Service;
+        this.TrackingNumber = TrackNumber;
+
+    }
+
+    public ShipmentDTO(int Id, ShipmentServiceEnum Service, int ShippingId, string TrackNumber, byte[] RowVersion ) 
+    {
+        this.Id = Id;
+        this.ShippingOrderID = ShippingId;
+        this.TrackingNumber = TrackNumber;
+        this.RowVersion = RowVersion;
+        this.Service = Service;
+    }
+
+
+
+    public enum ShipmentServiceEnum {
+        PUROLATOR, CANADAPOST, FEDEX
+    }
+
+
+
+
+
+}
