ffbb5130125af51ce77a5e8c776645155a777df6
Marc-Eric Boury <marc-eric.boury@collegelasalle.com>
2023-11-16 11:48:10 -0500

    ajout début services Prof

---
+++ Business/Services/RoleService.cs
@@ -0,0 +1,28 @@
+﻿using _420DA3_07451_Projet_Initial.Business.Abstracts;
+using _420DA3_07451_Projet_Initial.DataAccess.Contexts.Abstracts;
+using _420DA3_07451_Projet_Initial.DataAccess.DAOs;
+using _420DA3_07451_Projet_Initial.DataAccess.DAOs.Abstracts;
+using _420DA3_07451_Projet_Initial.DataAccess.DTOs;
+using _420DA3_07451_Projet_Initial.Presentation.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _420DA3_07451_Projet_Initial.Business.Services;
+public class RoleService : AbstractDtoService<Role, int> {
+    protected override RoleDAO Dao { get; }
+
+    protected override IDtoManagementView<Role> DtoManagementWindow { get; }
+
+
+    public RoleService(AbstractApplication parentApp, AbstractContext context) {
+        parentApp.RegisterDependent(this);
+        this.Dao = new RoleDAO(context);
+    }
+
+    public override void Shutdown() {
+        throw new NotImplementedException();
+    }
+}
---
+++ Business/Services/UtilisateurService.cs
@@ -0,0 +1,37 @@
+﻿using _420DA3_07451_Projet_Initial.Business.Abstracts;
+using _420DA3_07451_Projet_Initial.DataAccess.Contexts.Abstracts;
+using _420DA3_07451_Projet_Initial.DataAccess.DAOs;
+using _420DA3_07451_Projet_Initial.DataAccess.DAOs.Abstracts;
+using _420DA3_07451_Projet_Initial.DataAccess.DTOs;
+using _420DA3_07451_Projet_Initial.Presentation.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _420DA3_07451_Projet_Initial.Business.Services;
+public class UtilisateurService : AbstractDtoService<Utilisateur, int> {
+    protected override UtilisateurDAO Dao { get; }
+
+    protected override IDtoManagementView<Utilisateur> DtoManagementWindow { get; }
+
+    public UtilisateurService(AbstractFacade facade, AbstractContext context) {
+        facade.RegisterDependent(facade);
+        this.Dao = new UtilisateurDAO(context);
+
+    }
+
+
+
+
+
+    public override void Shutdown() {
+        try {
+            this.DtoManagementWindow.Dispose();
+        } catch (Exception ex) {
+            Debug.WriteLine("Failed to dispose of DtoManagementWindow on shutdown (possible memory leak): " + ex.Message);
+        }
+    }
+}
