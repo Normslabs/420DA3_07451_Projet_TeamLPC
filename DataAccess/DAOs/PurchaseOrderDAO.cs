06394a7ae8930f915ff7d4be10ae985b1bbe15b5
Wolfmen421 <portugal_2000f@hotmail.com>
2023-11-20 17:11:04 -0500

    Ajout de commentaire dans PurchaseOrderDAO et ProduitDAO

--- DataAccess/DAOs/PurchaseOrderDAO.cs
+++ DataAccess/DAOs/PurchaseOrderDAO.cs
@@ -8,6 +8,12 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace _420DA3_07451_Projet_Initial.DataAccess.DAOs;
+
+/*
+PurchaseOrder:
+GetIncompleteForWarehouse(intIdEntrepot OU objEntrepot) 
+-> retrourne les purchase orders ou le statut n'Est pas 'completed' et ou l'entrepot de destination = valeur passée
+*/
 public class PurchaseOrderDAO : AbstractDao<PurchaseOrder, int> {
 
     protected override AbstractContext Context { get; }
