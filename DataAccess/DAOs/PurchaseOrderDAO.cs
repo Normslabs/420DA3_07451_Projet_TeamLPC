2cfbbcfeae3a15b0a7b2bea57d4a8eb0606ec834
Wolfmen421 <portugal_2000f@hotmail.com>
2023-11-20 16:52:16 -0500

    PurchaseOrderDAO add

--- DataAccess/DAOs/PurchaseOrderDAO.cs
+++ DataAccess/DAOs/PurchaseOrderDAO.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿using _420DA3_07451_Projet_Initial.DataAccess.Contexts.Abstracts;
+using _420DA3_07451_Projet_Initial.DataAccess.DAOs.Abstracts;
+using _420DA3_07451_Projet_Initial.DataAccess.DTOs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace _420DA3_07451_Projet_Initial.DataAccess.DAOs;
-internal class PurchaseOrderDAO {
+public class PurchaseOrderDAO : AbstractDao<PurchaseOrder, int> {
+
+    protected override AbstractContext Context { get; }
+
+    public PurchaseOrderDAO(AbstractContext context) {
+        this.Context = context;
+    }
 
 
 
