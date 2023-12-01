21b03dbb5629bb47149b97eaab9d448ab44015f7
Wolfmen421 <portugal_2000f@hotmail.com>
2023-11-30 21:37:15 -0500

    Ajout de petit truc a la ProduitWindows Form

--- Presentation/ProduitWindow.Designer.cs
+++ Presentation/ProduitWindow.Designer.cs
@@ -41,11 +41,15 @@ partial class ProduitWindow {
             this.DeleteButton = new System.Windows.Forms.Button();
             this.ExitButton = new System.Windows.Forms.Button();
             this.produitListView = new System.Windows.Forms.ListView();
+            this.ProductNameTextBox = new System.Windows.Forms.TextBox();
+            this.ByNameButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ProductUPCTextBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // CreateButton
             // 
-            this.CreateButton.Location = new System.Drawing.Point(406, 43);
+            this.CreateButton.Location = new System.Drawing.Point(406, 239);
             this.CreateButton.Name = "CreateButton";
             this.CreateButton.Size = new System.Drawing.Size(167, 23);
             this.CreateButton.TabIndex = 0;
@@ -55,23 +59,23 @@ partial class ProduitWindow {
             // 
             // EditButton
             // 
-            this.EditButton.Location = new System.Drawing.Point(406, 131);
+            this.EditButton.Location = new System.Drawing.Point(405, 268);
             this.EditButton.Name = "EditButton";
             this.EditButton.Size = new System.Drawing.Size(167, 23);
             this.EditButton.TabIndex = 1;
             this.EditButton.Text = "Edit Selected Product";
             this.EditButton.UseVisualStyleBackColor = true;
-            this.EditButton.Click += new System.EventHandler(this.editButton_Click);
+            this.EditButton.Click += new System.EventHandler(this.EditButton_Click);
             // 
             // DeleteButton
             // 
-            this.DeleteButton.Location = new System.Drawing.Point(406, 214);
+            this.DeleteButton.Location = new System.Drawing.Point(405, 297);
             this.DeleteButton.Name = "DeleteButton";
             this.DeleteButton.Size = new System.Drawing.Size(167, 23);
             this.DeleteButton.TabIndex = 2;
             this.DeleteButton.Text = "Delete Selected Product";
             this.DeleteButton.UseVisualStyleBackColor = true;
-            this.DeleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
             // 
             // ExitButton
             // 
@@ -81,7 +85,7 @@ partial class ProduitWindow {
             this.ExitButton.TabIndex = 3;
             this.ExitButton.Text = "Exit";
             this.ExitButton.UseVisualStyleBackColor = true;
-            this.ExitButton.Click += new System.EventHandler(this.exitButton_Click);
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
             // 
             // produitListView
             // 
@@ -92,11 +96,47 @@ partial class ProduitWindow {
             this.produitListView.TabIndex = 4;
             this.produitListView.UseCompatibleStateImageBehavior = false;
             // 
+            // ProductNameTextBox
+            // 
+            this.ProductNameTextBox.Location = new System.Drawing.Point(405, 12);
+            this.ProductNameTextBox.Name = "ProductNameTextBox";
+            this.ProductNameTextBox.Size = new System.Drawing.Size(168, 23);
+            this.ProductNameTextBox.TabIndex = 5;
+            // 
+            // ByNameButton
+            // 
+            this.ByNameButton.Location = new System.Drawing.Point(406, 52);
+            this.ByNameButton.Name = "ByNameButton";
+            this.ByNameButton.Size = new System.Drawing.Size(167, 23);
+            this.ByNameButton.TabIndex = 6;
+            this.ByNameButton.Text = "Search Product by name";
+            this.ByNameButton.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(406, 134);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Search Product by UPC Code";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // ProductUPCTextBox
+            // 
+            this.ProductUPCTextBox.Location = new System.Drawing.Point(405, 94);
+            this.ProductUPCTextBox.Name = "ProductUPCTextBox";
+            this.ProductUPCTextBox.Size = new System.Drawing.Size(168, 23);
+            this.ProductUPCTextBox.TabIndex = 8;
+            // 
             // ProduitWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.ProductUPCTextBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ByNameButton);
+            this.Controls.Add(this.ProductNameTextBox);
             this.Controls.Add(this.produitListView);
             this.Controls.Add(this.ExitButton);
             this.Controls.Add(this.DeleteButton);
@@ -106,6 +146,7 @@ partial class ProduitWindow {
             this.Text = "ProduitWindow";
             this.Load += new System.EventHandler(this.ProduitWindow_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
     }
 
@@ -116,4 +157,8 @@ partial class ProduitWindow {
     private Button DeleteButton;
     private Button ExitButton;
     private ListView produitListView;
+    private TextBox ProductNameTextBox;
+    private Button ByNameButton;
+    private Button button1;
+    private TextBox ProductUPCTextBox;
 }
