aeeb68fd5687baa29b98f33c79b68ae92bf5f459
Marc-Eric Boury <meboury@hotmail.com>
2023-12-19 23:14:15 -0500

    table pivot

---
+++ DataAccess/DTOs/Pivots/ShippingOrderProducts.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _420DA3_07451_Projet_Initial.DataAccess.DTOs.Pivots;
+internal class ShippingOrderProducts {
+    public int ProduitId;
+    public int ShipmentOrderDTOId;
+    public int Quantite;
+
+    public ShippingOrderProducts() {
+
+    }
+
+    public ShippingOrderProducts(int produitId, int shipmentOrderDTOId, int quantite) {
+        this.ProduitId = produitId;
+        this.ShipmentOrderDTOId = shipmentOrderDTOId;
+        this.Quantite = quantite;
+    }   
+}
