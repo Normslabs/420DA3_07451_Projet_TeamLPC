67ef71ebc9b19e1a91b0ff5ed7fc8cd4f078166c
Marc-Eric Boury <meboury@hotmail.com>
2023-12-21 08:16:35 -0500

    clean-up Program.cs

--- Program.cs
+++ Program.cs
@@ -10,29 +10,6 @@ internal static class Program {
     /// </summary>
     [STAThread]
     public static void Main(string[] args) {
-        if (args.Contains("-profTests")) {
-
-            Debug.WriteLine(CryptographyService.HashPassword("potatoes"));
-            // Zone de tests pour le prof
-
-        } else if (args.Contains("-olivierTests")) {
-
-            // Zone de tests pour Olivier
-
-        } else if (args.Contains("-mouhamedTests")) {
-
-            // Zone de tests pour Mouhamed
-
-        } else if (args.Contains("-filipeTests")) {
-
-            ApplicationConfiguration.Initialize();
-
-        } else if (args.Contains("-maximilianoTests")) {
-
-            // Zone de tests pour Maximiliano
-
-        } else {
-            new GestionEntrepotApplication().Start();
-        }
+        new GestionEntrepotApplication().Start();
     }
 }
