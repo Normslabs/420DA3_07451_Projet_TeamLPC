4f679156426439c76ad6032e76305526b1ea5ce0
Marc-Eric Boury <meboury@hotmail.com>
2023-12-21 02:15:52 -0500

    fix migration

--- Migrations/AppDbContextModelSnapshot.cs
+++ Migrations/AppDbContextModelSnapshot.cs
@@ -25,21 +25,6 @@ namespace _420DA3_07451_Projet_Initial.Migrations
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
 
-            modelBuilder.Entity("RoleUtilisateur", b =>
-                {
-                    b.Property<int>("RolesId")
-                        .HasColumnType("int");
-
-                    b.Property<int>("UtilisateursPossedantRoleId")
-                        .HasColumnType("int");
-
-                    b.HasKey("RolesId", "UtilisateursPossedantRoleId");
-
-                    b.HasIndex("UtilisateursPossedantRoleId");
-
-                    b.ToTable("RoleUtilisateur");
-                });
-
             modelBuilder.Entity("_420DA3_07451_Projet_Initial.DataAccess.DTOs.Adresse", b =>
                 {
                     b.Property<int>("Id")
@@ -291,17 +276,17 @@ namespace _420DA3_07451_Projet_Initial.Migrations
 
                     b.Property<string>("EmailContact")
                         .IsRequired()
-                        .HasColumnType("nvarchar(128")
+                        .HasColumnType("nvarchar(128)")
                         .HasColumnName("EmailContact");
 
                     b.Property<string>("NomContact")
                         .IsRequired()
-                        .HasColumnType("nvarchar(20")
+                        .HasColumnType("nvarchar(20)")
                         .HasColumnName("NomContact");
 
                     b.Property<string>("PrenomContact")
                         .IsRequired()
-                        .HasColumnType("nvarchar(20")
+                        .HasColumnType("nvarchar(20)")
                         .HasColumnName("PrenomContact");
 
                     b.Property<byte[]>("RowVersion")
@@ -313,12 +298,12 @@ namespace _420DA3_07451_Projet_Initial.Migrations
 
                     b.Property<string>("SupplierName")
                         .IsRequired()
-                        .HasColumnType("nvarchar(100")
+                        .HasColumnType("nvarchar(100)")
                         .HasColumnName("NomFournisseur");
 
                     b.Property<string>("TelephoneContact")
                         .IsRequired()
-                        .HasColumnType("nvarchar(24")
+                        .HasColumnType("nvarchar(24)")
                         .HasColumnName("TelephoneContact");
 
                     b.HasKey("Id");
@@ -360,6 +345,40 @@ namespace _420DA3_07451_Projet_Initial.Migrations
                     b.HasIndex("ShipmentOrderDTOId");
 
                     b.ToTable("ShippingOrderProducts", (string)null);
+                });
+
+            modelBuilder.Entity("_420DA3_07451_Projet_Initial.DataAccess.DTOs.Pivots.UtilisateursRoles", b =>
+                {
+                    b.Property<int>("UtilisateurId")
+                        .HasColumnType("int")
+                        .HasColumnName("UtilisateurId");
+
+                    b.Property<int>("RoleId")
+                        .HasColumnType("int")
+                        .HasColumnName("RoleId");
+
+                    b.HasKey("UtilisateurId", "RoleId");
+
+                    b.HasIndex("RoleId");
+
+                    b.ToTable("RolesUtilisateurs", (string)null);
+
+                    b.HasData(
+                        new
+                        {
+                            UtilisateurId = 1,
+                            RoleId = 1
+                        },
+                        new
+                        {
+                            UtilisateurId = 1,
+                            RoleId = 2
+                        },
+                        new
+                        {
+                            UtilisateurId = 1,
+                            RoleId = 3
+                        });
                 });
 
             modelBuilder.Entity("_420DA3_07451_Projet_Initial.DataAccess.DTOs.Produit", b =>
@@ -426,6 +445,64 @@ namespace _420DA3_07451_Projet_Initial.Migrations
                     b.HasIndex("FournisseurId");
 
                     b.ToTable("Produits", (string)null);
+
+                    b.HasData(
+                        new
+                        {
+                            Id = 1,
+                            ClientsDTOId = 1,
+                            Description = "Un produit de test.",
+                            DoAutoRestock = true,
+                            FournisseurId = 1,
+                            InstockQuantity = 50,
+                            Name = "TestProduit",
+                            SupplierCode = "A535",
+                            TargetStockQuantity = 30,
+                            UpcCode = 15347634L,
+                            WeightInKg = 1.35m
+                        },
+                        new
+                        {
+                            Id = 2,
+                            ClientsDTOId = 1,
+                            Description = "Un produit de test.",
+                            DoAutoRestock = true,
+                            FournisseurId = 1,
+                            InstockQuantity = 50,
+                            Name = "TestProduit2",
+                            SupplierCode = "A534",
+                            TargetStockQuantity = 30,
+                            UpcCode = 15347635L,
+                            WeightInKg = 1.35m
+                        },
+                        new
+                        {
+                            Id = 3,
+                            ClientsDTOId = 1,
+                            Description = "Un produit de test.",
+                            DoAutoRestock = true,
+                            FournisseurId = 1,
+                            InstockQuantity = 50,
+                            Name = "TestProduit3",
+                            SupplierCode = "A533",
+                            TargetStockQuantity = 30,
+                            UpcCode = 15347636L,
+                            WeightInKg = 1.35m
+                        },
+                        new
+                        {
+                            Id = 4,
+                            ClientsDTOId = 1,
+                            Description = "Un produit de test.",
+                            DoAutoRestock = true,
+                            FournisseurId = 1,
+                            InstockQuantity = 50,
+                            Name = "TestProduit4",
+                            SupplierCode = "A532",
+                            TargetStockQuantity = 30,
+                            UpcCode = 15347637L,
+                            WeightInKg = 1.35m
+                        });
                 });
 
             modelBuilder.Entity("_420DA3_07451_Projet_Initial.DataAccess.DTOs.PurchaseOrder", b =>
@@ -504,6 +581,26 @@ namespace _420DA3_07451_Projet_Initial.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("Roles", (string)null);
+
+                    b.HasData(
+                        new
+                        {
+                            Id = 1,
+                            RoleDescription = "Role Administrateur",
+                            RoleName = "Administrator"
+                        },
+                        new
+                        {
+                            Id = 2,
+                            RoleDescription = "Employés de bureau",
+                            RoleName = "OfficeEmployee"
+                        },
+                        new
+                        {
+                            Id = 3,
+                            RoleDescription = "Employés d'entrepôt",
+                            RoleName = "WarehouseEmployee"
+                        });
                 });
 
             modelBuilder.Entity("_420DA3_07451_Projet_Initial.DataAccess.DTOs.ShipmentDTO", b =>
@@ -658,21 +755,16 @@ namespace _420DA3_07451_Projet_Initial.Migrations
                         .IsUnique();
 
                     b.ToTable("Utilisateurs", (string)null);
-                });
 
-            modelBuilder.Entity("RoleUtilisateur", b =>
-                {
-                    b.HasOne("_420DA3_07451_Projet_Initial.DataAccess.DTOs.Role", null)
-                        .WithMany()
-                        .HasForeignKey("RolesId")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
-
-                    b.HasOne("_420DA3_07451_Projet_Initial.DataAccess.DTOs.Utilisateur", null)
-                        .WithMany()
-                        .HasForeignKey("UtilisateursPossedantRoleId")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
+                    b.HasData(
+                        new
+                        {
+                            Id = 1,
+                            DateCreated = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                            EntrepotDeTravailId = 1,
+                            PasswordHash = "AC72E9D92E94CEC187922736EFE36904643D6267C63D6AB84752D62C1C2817F5:80E423C38FB59F822BAF448F8A8943E7:100000:SHA256",
+                            Username = "testUser"
+                        });
                 });
 
             modelBuilder.Entity("_420DA3_07451_Projet_Initial.DataAccess.DTOs.ClientsDTO", b =>
@@ -680,13 +772,13 @@ namespace _420DA3_07451_Projet_Initial.Migrations
                     b.HasOne("_420DA3_07451_Projet_Initial.DataAccess.DTOs.Entrepot", "AsignedWarehouse")
                         .WithMany("Clients")
                         .HasForeignKey("AsignedWarehouseID")
-                        .OnDelete(DeleteBehavior.Cascade)
+                        .OnDelete(DeleteBehavior.Restrict)
                         .IsRequired();
 
                     b.HasOne("_420DA3_07451_Projet_Initial.DataAccess.DTOs.Adresse", "ClientAdress")
                         .WithOne("Client")
                         .HasForeignKey("_420DA3_07451_Projet_Initial.DataAccess.DTOs.ClientsDTO", "ClientAdressId")
-                        .OnDelete(DeleteBehavior.Cascade)
+                        .OnDelete(DeleteBehavior.Restrict)
                         .IsRequired();
 
                     b.Navigation("AsignedWarehouse");
@@ -699,7 +791,7 @@ namespace _420DA3_07451_Projet_Initial.Migrations
                     b.HasOne("_420DA3_07451_Projet_Initial.DataAccess.DTOs.Adresse", "AdresseEntrepot")
                         .WithOne("AdresseEntrepot")
                         .HasForeignKey("_420DA3_07451_Projet_Initial.DataAccess.DTOs.Entrepot", "AdresseId")
-                        .OnDelete(DeleteBehavior.Cascade)
+                        .OnDelete(DeleteBehavior.Restrict)
                         .IsRequired();
 
                     b.Navigation("AdresseEntrepot");
@@ -710,7 +802,7 @@ namespace _420DA3_07451_Projet_Initial.Migrations
                     b.HasOne("_420DA3_07451_Projet_Initial.DataAccess.DTOs.Adresse", "SupplierAdresse")
                         .WithOne("AdresseFournisseur")
                         .HasForeignKey("_420DA3_07451_Projet_Initial.DataAccess.DTOs.Fournisseur", "AdresseId")
-                        .OnDelete(DeleteBehavior.Cascade)
+                        .OnDelete(DeleteBehavior.Restrict)
                         .IsRequired();
 
                     b.Navigation("SupplierAdresse");
@@ -721,13 +813,13 @@ namespace _420DA3_07451_Projet_Initial.Migrations
                     b.HasOne("_420DA3_07451_Projet_Initial.DataAccess.DTOs.Produit", "Produit")
                         .WithMany("ShippingOrderProducts")
                         .HasForeignKey("ProduitId")
-                        .OnDelete(DeleteBehavior.Cascade)
+                        .OnDelete(DeleteBehavior.Restrict)
                         .IsRequired();
 
                     b.HasOne("_420DA3_07451_Projet_Initial.DataAccess.DTOs.ShipmentOrderDTO", "ShipmentOrderDTO")
                         .WithMany("AssociationsProduits")
                         .HasForeignKey("ShipmentOrderDTOId")
-                        .OnDelete(DeleteBehavior.Cascade)
+                        .OnDelete(DeleteBehavior.Restrict)
                         .IsRequired();
 
                     b.Navigation("Produit");
@@ -735,18 +827,37 @@ namespace _420DA3_07451_Projet_Initial.Migrations
                     b.Navigation("ShipmentOrderDTO");
                 });
 
+            modelBuilder.Entity("_420DA3_07451_Projet_Initial.DataAccess.DTOs.Pivots.UtilisateursRoles", b =>
+                {
+                    b.HasOne("_420DA3_07451_Projet_Initial.DataAccess.DTOs.Role", "Role")
+                        .WithMany()
+                        .HasForeignKey("RoleId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.HasOne("_420DA3_07451_Projet_Initial.DataAccess.DTOs.Utilisateur", "Utilisateur")
+                        .WithMany()
+                        .HasForeignKey("UtilisateurId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("Role");
+
+                    b.Navigation("Utilisateur");
+                });
+
             modelBuilder.Entity("_420DA3_07451_Projet_Initial.DataAccess.DTOs.Produit", b =>
                 {
                     b.HasOne("_420DA3_07451_Projet_Initial.DataAccess.DTOs.ClientsDTO", "ClientsDTO")
                         .WithMany("Produit")
                         .HasForeignKey("ClientsDTOId")
-                        .OnDelete(DeleteBehavior.Cascade)
+                        .OnDelete(DeleteBehavior.Restrict)
                         .IsRequired();
 
                     b.HasOne("_420DA3_07451_Projet_Initial.DataAccess.DTOs.Fournisseur", "Fournisseur")
                         .WithMany("ProduitsFournis")
                         .HasForeignKey("FournisseurId")
-                        .OnDelete(DeleteBehavior.Cascade)
+                        .OnDelete(DeleteBehavior.Restrict)
                         .IsRequired();
 
                     b.Navigation("ClientsDTO");
@@ -759,13 +870,13 @@ namespace _420DA3_07451_Projet_Initial.Migrations
                     b.HasOne("_420DA3_07451_Projet_Initial.DataAccess.DTOs.Entrepot", "DestinationWarehouse")
                         .WithMany("PurchaseOrders")
                         .HasForeignKey("DestinationWarehouseID")
-                        .OnDelete(DeleteBehavior.Cascade)
+                        .OnDelete(DeleteBehavior.Restrict)
                         .IsRequired();
 
                     b.HasOne("_420DA3_07451_Projet_Initial.DataAccess.DTOs.Produit", "Product")
                         .WithMany("PurchaseOrders")
                         .HasForeignKey("ProductId")
-                        .OnDelete(DeleteBehavior.Cascade)
+                        .OnDelete(DeleteBehavior.Restrict)
                         .IsRequired();
 
                     b.Navigation("DestinationWarehouse");
@@ -777,7 +888,8 @@ namespace _420DA3_07451_Projet_Initial.Migrations
                 {
                     b.HasOne("_420DA3_07451_Projet_Initial.DataAccess.DTOs.ShipmentOrderDTO", "ShippingOrder")
                         .WithOne("Shipment")
-                        .HasForeignKey("_420DA3_07451_Projet_Initial.DataAccess.DTOs.ShipmentDTO", "ShippingOrderID");
+                        .HasForeignKey("_420DA3_07451_Projet_Initial.DataAccess.DTOs.ShipmentDTO", "ShippingOrderID")
+                        .OnDelete(DeleteBehavior.Restrict);
 
                     b.Navigation("ShippingOrder");
                 });
@@ -787,17 +899,18 @@ namespace _420DA3_07451_Projet_Initial.Migrations
                     b.HasOne("_420DA3_07451_Projet_Initial.DataAccess.DTOs.ClientsDTO", "Clients")
                         .WithMany("ShipmentOrders")
                         .HasForeignKey("ClientsId")
-                        .OnDelete(DeleteBehavior.Cascade)
+                        .OnDelete(DeleteBehavior.Restrict)
                         .IsRequired();
 
                     b.HasOne("_420DA3_07451_Projet_Initial.DataAccess.DTOs.Utilisateur", "EmployeEntrepot")
                         .WithMany("AssignedShipmentOrders")
-                        .HasForeignKey("EmployeEntrepotId");
+                        .HasForeignKey("EmployeEntrepotId")
+                        .OnDelete(DeleteBehavior.Restrict);
 
                     b.HasOne("_420DA3_07451_Projet_Initial.DataAccess.DTOs.Entrepot", "EntrepotOriginal")
                         .WithMany("ShipmentOrder")
                         .HasForeignKey("EntrepotOriginalId")
-                        .OnDelete(DeleteBehavior.Cascade)
+                        .OnDelete(DeleteBehavior.Restrict)
                         .IsRequired();
 
                     b.Navigation("Clients");
